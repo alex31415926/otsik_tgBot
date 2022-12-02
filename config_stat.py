c9fb3adc7db2b6e9cce1ba49727f659701339808
split_ua <split_ua@mail.ru>
2022-12-02 15:56:31 +0300

    добавлена web-часть
    две страницы с обновляемой статистикой
    index.php - общая статистика
    index2.php - выборочная статистика по ролям

--- config_stat.py
+++ config_stat.py
@@ -1,2 +1,3 @@
 results_data_patch = r"e:\_py\bots\_MariBot\results"
 result_file = r"e:\_Denver\denwer\www\denwer\bot\stat"
+result_file2 = r"e:\_Denver\denwer\www\denwer\bot\stat2"
---
+++ web/index.php
@@ -0,0 +1,253 @@
+<html>
+
+<head>
+
+<title>Компания "Торговый мир"</title>
+
+</head>
+
+<body>
+
+<?
+error_reporting(E_ALL);
+$run2 = "E: && cd e:\_py\bots\_MariBot && python stat_count.py";
+exec($run2);
+
+$fh = fopen('stat','r');
+$all = fgets($fh);
+$tmp1 = explode(" ",fgets($fh));
+$tmp2 = explode(" ",fgets($fh));
+$tmp3 = explode(" ",fgets($fh));
+$tmp4 = explode(" ",fgets($fh));
+$tmp5 = explode(" ",fgets($fh));
+$tmp6 = explode(" ",fgets($fh));
+$tmp7 = explode(" ",fgets($fh));
+$tmp8 = explode(" ",fgets($fh));
+$tmp9 = explode(" ",fgets($fh));
+$tmp10 = explode(" ",fgets($fh));
+$tmp11 = explode(" ",fgets($fh));
+$tmp12 = explode(" ",fgets($fh));
+$tmp13 = explode(" ",fgets($fh));
+
+$school = explode(" ",fgets($fh)); //добавить все роли
+
+?>  
+
+<h1>Статистика</h1>
+<h1>Методика пройдена <?=$all?> раз</h1>
+
+<form action="http://127.0.0.1/denwer/bot/index2.php" method="post">
+   <p><select name="role" size="7" multiple>
+    	<option selected value="role_1">Студент</option>
+   	<option value="role_2">Школьник</option>
+   	<option value="role_3">Гос. служащий</option>
+   	<option value="role_4">Пенсионер</option>
+   	<option value="role_5">Фрилансер</option>
+	<option value="role_6">Безработный</option>
+	<option value="role_7">Домохозяйка</option>
+   </select>
+   <input type="submit" value="Отправить"></p> 
+</form>
+
+<br>
+<b>Таблица распределения дисфункции и компенсаторных механизмов по шкалам опросника</b>.<br>
+
+<table border="2" align="center" cellpadding="10">
+
+<tr>
+
+<td bgcolor="yellow" align="center"><b>Шкала</b></td>
+
+<td bgcolor="yellow" align="center"><b>Дисфункция</b></td>
+
+<td bgcolor="yellow" align="center"><b>Норма</b></td>
+
+<td bgcolor="yellow" align="center"><b>Компенсация</b></td>
+
+</tr>
+
+<tr>
+
+<td align="center">1. Выраженность аффективных переживаний субъекта и его отношения к ним</td>
+
+<td align="center"><?= $tmp1[0]?> (<?= number_format(($tmp1[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp1[0] - $tmp1[1] ?> (<?= number_format((($all - $tmp1[0] - $tmp1[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp1[1]?> (<?= number_format(($tmp1[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">2. Степень опредмеченности желания субъекта</td>
+
+<td align="center"><?= $tmp2[0]?> (<?= number_format(($tmp2[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp2[0] - $tmp1[1] ?> (<?= number_format((($all - $tmp2[0] - $tmp2[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp2[1]?> (<?= number_format(($tmp2[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">3. Cтепень активности взаимодействия субъекта с реальностью</td>
+
+<td align="center"><?= $tmp3[0]?> (<?= number_format(($tmp3[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp3[0] - $tmp3[1] ?> (<?= number_format((($all - $tmp3[0] - $tmp3[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp3[1]?> (<?= number_format(($tmp3[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">4. Умение прогнозировать результат предпринимаемых действий</td>
+
+<td align="center"><?= $tmp4[0]?> (<?= number_format(($tmp4[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp4[0] - $tmp4[1] ?> (<?= number_format((($all - $tmp4[0] - $tmp4[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp4[1]?> (<?= number_format(($tmp4[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">5. Межличностное взаимодействие субъекта</td>
+
+<td align="center"><?= $tmp5[0]?> (<?= number_format(($tmp5[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp5[0] - $tmp5[1] ?> (<?= number_format((($all - $tmp5[0] - $tmp5[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp5[1]?> (<?= number_format(($tmp5[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">6. Cостояние самооценки субъекта</td>
+
+<td align="center"><?= $tmp6[0]?> (<?= number_format(($tmp6[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp6[0] - $tmp6[1] ?> (<?= number_format((($all - $tmp6[0] - $tmp6[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp6[1]?> (<?= number_format(($tmp6[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">7. Отношение к негативному опыту и степень фиксированности на имеющихся затруднениях</td>
+
+<td align="center"><?= $tmp7[0]?> (<?= number_format(($tmp7[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp7[0] - $tmp7[1] ?> (<?= number_format((($all - $tmp7[0] - $tmp7[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp7[1]?> (<?= number_format(($tmp7[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+</table>
+
+<b>Таблица распределения дисфункции и компенсации по механизмам методики</b>.<br>
+
+<table border="2" align="center" cellpadding="10">
+
+<tr>
+
+<td bgcolor="yellow" align="center"><b>Механизм</b></td>
+
+<td bgcolor="yellow" align="center"><b>Дисфункция</b></td>
+
+<td bgcolor="yellow" align="center"><b>Норма</b></td>
+
+<td bgcolor="yellow" align="center"><b>Компенсация</b></td>
+
+</tr>
+
+<tr>
+
+<td align="center">самоопредмечивания</td>
+
+<td align="center"><?= $tmp8[0]?> (<?= number_format(($tmp8[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp8[0] - $tmp8[1] ?> (<?= number_format((($all - $tmp8[0] - $tmp8[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp8[1]?> (<?= number_format(($tmp8[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">самооценивания</td>
+
+<td align="center"><?= $tmp9[0]?> (<?= number_format(($tmp9[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp9[0] - $tmp9[1] ?> (<?= number_format((($all - $tmp9[0] - $tmp9[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp9[1]?> (<?= number_format(($tmp9[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">самоформирования</td>
+
+<td align="center"><?= $tmp10[0]?> (<?= number_format(($tmp10[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp10[0] - $tmp10[1] ?> (<?= number_format((($all - $tmp10[0] - $tmp10[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp10[1]?> (<?= number_format(($tmp10[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">самоотеждествления</td>
+
+<td align="center"><?= $tmp11[0]?> (<?= number_format(($tmp11[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp11[0] - $tmp11[1] ?> (<?= number_format((($all - $tmp11[0] - $tmp11[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp11[1]?> (<?= number_format(($tmp11[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">самоопределения</td>
+
+<td align="center"><?= $tmp12[0]?> (<?= number_format(($tmp12[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp12[0] - $tmp12[1] ?> (<?= number_format((($all - $tmp12[0] - $tmp12[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp12[1]?> (<?= number_format(($tmp12[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+<tr>
+
+<td align="center">самоодобрения</td>
+
+<td align="center"><?= $tmp13[0]?> (<?= number_format(($tmp13[0]*100/$all), 2)?>%)</td>
+
+<td align="center"><?=$all - $tmp13[0] - $tmp13[1] ?> (<?= number_format((($all - $tmp13[0] - $tmp13[1])*100/$all), 2)?>%)</td>
+
+<td align="center"><?= $tmp13[1]?> (<?= number_format(($tmp13[1]*100/$all), 2)?>%)</td>
+
+</tr>
+
+</table>
+
+<?php
+fclose($fh); 
+?>
+
+Более подробно об ассортименте товаров и услуг вы можете узнать в нашем отделе сбыта.
+
+</body>
+
+</html>
